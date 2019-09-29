7ee50aa2b946414206def3db16ebf231658dda3c
algirdasx <algirdas.n@gmail.com>
2019-09-29 14:36:30 +0300

    test function

--- main.ts
+++ main.ts
@@ -1,7 +1,14 @@
-basic.showLeds(`
-    . . . . .
-    . # . # .
-    . . . . .
-    # . . . #
-    . # # # .
-    `);
+/**
+ * Provides access to basic micro:bit functionality.
+ */
+//% color=190 weight=100 icon="\uf1ec" block="Basic Blocks"
+namespace test {
+
+    //% block
+    export function showNumber(v: number, interval: number = 150): void
+    { }
+
+
+}
+
+
